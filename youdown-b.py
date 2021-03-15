95a4b6a2f7ae3aa254d01cd69054bcd644cecc08
Scripts en Python <79738875+Balta-Python@users.noreply.github.com>
2021-03-14 22:53:53 -0700

    Update youdown-b.py

--- youdown-b.py
+++ youdown-b.py
@@ -1,84 +1,40 @@
-from colorama import Fore, Style
-import pafy
+from pytube import YouTube
+from tqdm import tqdm
+import time
+from colorama import init, Fore
 import os
+
+loop = tqdm(total = 50000, position=0, leave=False)
+for k in range(50000):
+    loop.set_description('Ejecutando script'.format(k))
+    loop.update(1)
+loop.close()
 
 os.system("clear")
 
-print(Fore.LIGHTYELLOW_EX + "\n◈ ━━━━━━━━━━ ◆ ━━━━━━━━━━ ◈")
-print( "╔═╦╗   ╔══╗          ╔══╗")
-print("╚╗║╠═╦╦╬╗╗╠═╦╦╦╦═╦╦══╣╔╗║")
-print("╔╩╗║╬║║╠╩╝║╬║║║║║║╠══╣╔╗║")
-print("╚══╩═╩═╩══╩═╩══╩╩═╝  ╚══╝")
+while True:
+    print(Fore.YELLOW + "\n◈ ━━━━━━━━━━ ◆ ━━━━━━━━━━ ◈")
+    print(Fore.MAGENTA + "╔═╦╗   ╔══╗          ╔══╗")
+    print(Fore.BLUE + "╚╗║╠═╦╦╬╗╗╠═╦╦╦╦═╦╦══╣╔╗║")
+    print(Fore.GREEN + "╔╩╗║╬║║╠╩╝║╬║║║║║║╠══╣╔╗║")
+    print(Fore.RED + "╚══╩═╩═╩══╩═╩══╩╩═╝  ╚══╝")
 
-print(Fore.YELLOW + "1", end="")
-print(Fore.GREEN + ") ", end="")
-print(Fore.BLUE + "D", end="")
-print(Fore.RED + "e", end="")
-print(Fore.CYAN + "s", end="")
-print(Fore.MAGENTA + "c", end="")
-print(Fore.WHITE + "a", end="")
-print(Fore.YELLOW + "r", end="")
-print(Fore.RED + "g", end="")
-print(Fore.YELLOW + "a", end="")
-print(Fore.GREEN + "r", end="")
-print(Fore.BLUE + " V", end="")
-print(Fore.YELLOW + "i", end="")
-print(Fore.GREEN + "d", end="")
-print(Fore.YELLOW + "e", end="")
-print(Fore.GREEN + "o")
-print(Fore.BLUE + "2", end="")
-print(Fore.RED + ")", end="")
-print(Fore.CYAN + "D", end="")
-print(Fore.MAGENTA + "e", end="")
-print(Fore.WHITE + "s", end="")
-print(Fore.YELLOW + "c", end="")
-print(Fore.RED + "a", end="")
-print(Fore.YELLOW + "r", end="")
-print(Fore.GREEN + "g", end="")
-print(Fore.BLUE + "a", end="")
-print(Fore.YELLOW + "r ", end="")
-print(Fore.GREEN + "A", end="")
-print(Fore.CYAN + "u", end="")
-print(Fore.MAGENTA + "d", end="")
-print(Fore.WHITE + "i", end="")
-print(Fore.YELLOW + "o")
-print(Fore.WHITE + "3", end="")
-print(Fore.YELLOW + ") ", end="")
-print(Fore.RED + "S", end="")
-print(Fore.YELLOW + "a", end="")
-print(Fore.GREEN + "l", end="")
-print(Fore.BLUE + "i", end="")
-print(Fore.YELLOW + "r", end="")
-selec = input("Seleccione: ")
+    print(Fore.YELLOW + "C", end="")
+    print(Fore.GREEN + "o", end="")
+    print(Fore.BLUE + "d", end="")
+    print(Fore.RED + "e", end="")
+    print(Fore.CYAN + "d ", end="")
+    print(Fore.MAGENTA + "b", end="")
+    print(Fore.WHITE + "y ", end="")
+    print(Fore.YELLOW + "B", end="")
+    print(Fore.RED + "a", end="")
+    print(Fore.YELLOW + "l", end="")
+    print(Fore.GREEN + "t", end="")
+    print(Fore.MAGENTA + "a", end="")
 
-if selec == "1":
-    # URL
-    download_url = input(Style.BRIGHT + Fore.RED + "URL: " + Style.NORMAL + Fore.WHITE)
-    video = pafy.new(download_url)
-
-    streams = video.streams
-
-    for i in streams:
-        print(i)
-
-    # Mejor resolucion
-    best = video.getbest()
-    print(best.resolution, best.extension)
-
-    # Descarga - Directorio
-    best.download(filepath="video")
-    print(Fore.RED + "Video descargado correctamente!")
-
-elif selec == "2":
-    download_url = input(Style.BRIGHT + Fore.RED + "URL: " + Style.NORMAL + Fore.WHITE)
-    video = pafy.new(download_url)
-
-    audiostreams = video.audiostreams
-    for i in audiostreams:
-        print(i.bitrate, i.extension, i.get_filesize())
-
-    audiostreams[1].download()
-    print(Fore.RED + "Audio descargado correctamente!")
-
-else:
-    exit()
+    url = input(Fore.YELLOW + "\n>>> Enter the URL of the video: ")
+    print("\n(･ิᴗ･ิ)", "Downloading Video...")
+    YouTube(url).streams.get_highest_resolution().download()
+    YouTube(url).streams.first().download()
+    print("\n( ^-^)/", "¡Your video has been downloaded successfully!")
+    time.sleep(4)
